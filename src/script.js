01b027727a71ffede099bb9a1a235a291dd62c56
Rhitree <rhian.jenkins90@gmail.com>
2024-09-30 16:10:14 +0100

    Added api intergration

--- src/script.js
+++ src/script.js
@@ -1,14 +1,27 @@
+function displayLyrics(response) {
+    new Typewriter("#lyrics", {
+        strings: response.data.answer,
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+});
+}
+
 function generateLyrics(event) {
     event.preventDefault();
 
-new Typewriter("#lyrics", {
-    strings: ["You've got to get up every morning, with a smile on your face and show the world, all the love in your heart."],
-    autoStart: true,
-    delay: 1,
-    cursor: "",
-});
+let instructionsInput = document.querySelector(`#user-instructions`);
+let apiKey = `b3a37c1584b0oatf80a196c74f3071cb`;
+let prompt = `You are a talented pop song writer. Your mission is to generate a song with 2 verses, each with 4 lines, and a 4 line chorus in basic HTML. Separate each line with a </br> and separate the verses and chorus with </br> </br>.  Make sure to follow the user instructions. Do not include a title to the song. Sign the song with 'SheCodes AI' inside a <strong> element at the end of the song and NOT at the beginning`;
+let context = `User instructions: Generate an English pop song about ${instructionsInput.value}`;
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+let lyricsElement = document.querySelector("#lyrics");
+lyricsElement.classList.remove(`hidden`);
+lyricsElement.innerHTML = `<div class="generating">⏳ Generating a song about ${instructionsInput.value}</div>`;
+
+axios.get(apiUrl).then(displayLyrics);
 }
 
 let lyricFormElement = document.querySelector("#lyric-generator");
 lyricFormElement.addEventListener("submit", generateLyrics);
-
