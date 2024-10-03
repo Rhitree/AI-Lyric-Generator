81fe149e3f4a15155fd4a5837d1db8090d2b100a
Rhitree <rhian.jenkins90@gmail.com>
2024-10-03 11:23:56 +0100

    Hid lyrics result using js and css until topic is chosen by user. Added final css design

--- src/script.js
+++ src/script.js
@@ -12,12 +12,12 @@ function generateLyrics(event) {
 
 let instructionsInput = document.querySelector(`#user-instructions`);
 let apiKey = `b3a37c1584b0oatf80a196c74f3071cb`;
-let prompt = `You are a talented pop song writer. Your mission is to generate a song with 2 verses, each with 4 lines, and a 4 line chorus in basic HTML. Separate each line with a </br> and separate the verses and chorus with </br> </br>.  Make sure to follow the user instructions. Do not include a title to the song. Sign the song with 'SheCodes AI' inside a <strong> element at the end of the song and NOT at the beginning`;
+let prompt = `You are a talented pop song writer. Your mission is to generate a song with 2 verses, each with 4 lines, and a 4 line chorus in basic HTML, only display the lyrics. Separate each line with a </br> and separate the verses and chorus with </br> </br>.  Make sure to follow the user instructions. Do not include a title to the song. Sign the song with '-SheCodes AI' inside a <strong> element at the end of the song and NOT at the beginning`;
 let context = `User instructions: Generate an English pop song about ${instructionsInput.value}`;
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 let lyricsElement = document.querySelector("#lyrics");
-lyricsElement.classList.remove(`hidden`);
+lyricsElement.classList.remove("hidden");
 lyricsElement.innerHTML = `<div class="generating">⏳ Generating a song about ${instructionsInput.value}</div>`;
 
 axios.get(apiUrl).then(displayLyrics);
